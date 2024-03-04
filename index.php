d26add48e4aa6858c6c487ddf726cde627343e2a
AntonioDiBari <antoniodibari.dev@gmail.com>
2024-03-04 15:58:05 +0100

    fix: gestito caso voto non da 1 a 10 feat: calcolo percentuale gradimento dal voto con function

--- index.php
+++ index.php
@@ -9,20 +9,29 @@ class Production
     function __construct(
         string $titleParam,
         string $langParam,
-        float $voteParam
+        $voteParam,
     ) {
         $this->title = $titleParam;
         $this->lang = $langParam;
-        $this->vote = $voteParam;
+        $this->vote = ($voteParam >= 0 && $voteParam <= 10) ? $voteParam : "Immettere nei parametri voto da 1 a 10";
     }
 
     public function getPercentage()
     {
-        return $this->vote * 100 / 10;
+        if (gettype($this->vote) == "double" || gettype($this->vote) == "integer") {
+            $percentage = $this->vote * 100 / 10;
+            return $this->title . " ha avuto un gradimento del " . $percentage . "%";
+        } else {
+            return "Metodo non applicabile, voto non da 1 a 10";
+        }
+
     }
 }
 
 $gameOfThrones = new Production("Game of Thrones", "ENG", 8.9);
-$lost = new Production("Lost", "ENG", 8.3);
-var_dump($gameOfThrones, $gameOfThrones->title . " ha avuto un gradimento del " . $gameOfThrones->getPercentage() . "%");
-var_dump($lost, $lost->title . " ha avuto un gradimento del " . $lost->getPercentage() . "%");
+$lost = new Production("Lost", "ENG", 8);
+
+var_dump($gameOfThrones, $gameOfThrones->getPercentage());
+var_dump($lost, $lost->getPercentage());
+
+$productions = [$gameOfThrones, $lost];
